e6614875d1bbce3f8f095beb006a3c25d4d1a36f
Sergii Balog <sbalog@lanteria.net>
2019-11-21 20:29:10 +0200

    datatableSupportAdded

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
 import { PostsService } from "../services/posts.service";
 import * as $ from "jquery";
+import "datatables.net";
+import "datatables.net-bs4";
 
 @Component({
   selector: "posts",
@@ -10,16 +12,60 @@ import * as $ from "jquery";
 export class PostsComponent implements OnInit {
   posts: any[];
 
-  constructor(private service: PostsService) {}
+  constructor(
+    private service: PostsService,
+    private changeReference: ChangeDetectorRef
+  ) {}
   ngOnInit(): void {
+    this.loadDataTableDynamically();
+    // this.service.addService({}).subscribe(resp => {
+    //   console.log(resp);
+    // });
+    window["deletePost"] = this.deletePost;
+  }
+  public deletePost(id) {
+    if (confirm("Are you sure you want to delete post?")) {
+      alert(id + " post deleted");
+    }
+  }
+  loadDataTableStatically() {
     this.service.getPosts().subscribe(resp => {
       // resp.headers
       //   .keys()
       //   .map(x => console.log(`${x}: ${resp.headers.get(x)}`));
       this.posts = resp.body;
+
+      this.changeReference.detectChanges();
+
+      const table: any = $("table");
+      table.DataTable();
     });
-    // this.service.addService({}).subscribe(resp => {
-    //   console.log(resp);
-    // });
+  }
+  loadDataTableDynamically() {
+    const table: any = $("#tableData");
+
+    table.DataTable({
+      ajax: {
+        url: "http://jsonplaceholder.typicode.com/posts",
+        type: "GET",
+        datatype: "json",
+        dataSrc: ""
+      },
+      columns: [
+        { data: "title", width: "30%" },
+        { data: "body", width: "60%" },
+        {
+          data: "id",
+          width: "10%",
+          render: function(data, type, row) {
+            return `<button onclick="deletePost(${data})">Delete</button>`;
+          }
+        }
+      ],
+      language: {
+        emptyTable: "No records found"
+      },
+      width: "100%"
+    });
   }
 }
