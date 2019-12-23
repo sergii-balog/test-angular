fa018b26e0bb3b1322829cfa796adb7c57bc46ce
Sergii Balog <sbalog@lanteria.net>
2019-12-23 19:09:22 +0200

    theming added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  sanitizer: DomSanitizer;
+  theme: string;
+  constructor(_sanitizer: DomSanitizer) {
+    this.sanitizer = _sanitizer;
+  }
+  ngOnInit() {
+    this.theme = "/assets/themes/1.css";
+  }
+  changeTheme() {
+    if (this.theme === "/assets/themes/1.css") {
+      this.theme = "/assets/themes/2.css";
+    } else if (this.theme === "/assets/themes/2.css") {
+      this.theme = "/assets/themes/3.css";
+    } else {
+      this.theme = "/assets/themes/1.css";
+    }
+  }
+}
